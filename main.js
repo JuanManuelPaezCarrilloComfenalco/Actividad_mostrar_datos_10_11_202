bd28f1ae5998c6efc2bb9f02da4ac1b2f8d7cbbb
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-11-11 15:31:12 -0500

    Actividad_mostrar_datos_10_11_2022

---
+++ main.js
@@ -0,0 +1,22 @@
+addEventListener(`DOMContentLoaded`, (e) => {
+    
+    alert(`Hola, USUARIO`);
+    alert(`como estas? ¡BIENVENIDO!`);
+    alert(`Según mi base de datos eres JUAN PAEZ, tienes 38 años, vives en Lebrija y estudias JavaScript.`);
+    alert(`Compartenos un poco mas de informacion por favor`)
+    
+    console.log("¿cual es tu espectativa en cuanto a este diplomado?");
+    console.info("Rta: profundizar en esta rama para poder independizarme a corto plazo")
+    
+    console.log(`¿Como piensas conseguir tu meta?`);
+    console.info(`Rta: inicialmente ofreciendo mis servicios a pequeñas y medianas empresas con recursos suficientes de manera que puedan pagar el servicio`);
+
+    console.log(`¿Como piensas llegar al mercado global?`);
+    console.info(`Rta: Permaneciendo informado e investigando constantemente sobre la demanda a nivel mundial`);
+    
+    console.log(`¿Cuales son tus espectaivas en esta profesión?`);
+    console.info(`Rta: Estudio programación porque me parece una carrera interesante y me gusta, pienso que es lo mas importante`);
+        
+    document.querySelector("body").innerHTML ="INFORMACION JUAN PAEZ EN LA CONSOLA, Oprimir f 12 =>";
+    
+    })
